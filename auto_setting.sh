90cf12ed7ea30e4c2a570911d032e17ebe3d8266
Jamosf <271917994@qq.com>
2018-09-08 01:28:33 -0700

    modify

--- auto_setting.sh
+++ auto_setting.sh
@@ -1,8 +1,22 @@
 #! /bin/bash
 
-ln -s ~/vimforubuntu/.tmux.conf ~/.tmux.conf
-ln -s ~/vimforubuntu/.vimrc  ~/.vimrc
-ln -s ~/vimforubuntu/.vimrc  ~/.vimrc
-ln -s ~/vimforubuntu/.tmux ~/.tmux
+#install something indeed
+apt-get update
+go get -u github.com/jstemmer/gotags
+sudo apt-get install ctags -y
+sudo apt-get install ack-grep -y
+sudo apt-get install tmux -y
+sudo apt-get install build-essential cmake -y
+
+cd ~/vim-bash-tmux/.vim/bundle/YouCompleteMe/
+./install.py --clang-completer
+./install.py --go-completer
+
+
+
+ln -s ~/vim-bash-tmux/.tmux.conf ~/.tmux.conf
+ln -s ~/vim-bash-tmux/.vimrc  ~/.vimrc
+ln -s ~/vim-bash-tmux/.vimrc  ~/.vimrc
+ln -s ~/vim-bash-tmux/.tmux ~/.tmux
 rm -rf ~/.bashrc
-ln -s ~/vimforubuntu/.bashrc ~/.bashrc 
+ln -s ~/vim-bash-tmux/.bashrc ~/.bashrc 
